5de8209fc0c9c6a8d48d946deb2524be2374605d
Ram Niwash Pandey <pandey.ram1397@gmail.com>
2025-06-26 00:22:22 +0530

    Exploring get,post,put,patch,delete

--- src/app.js
+++ src/app.js
@@ -3,12 +3,34 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+
+// app.use("/test", (req, res) => {
+//     res.send("Hello from Test Server!")
+// })
+// app.use("/hello", (req, res) => {
+//     res.send("Hello from Hello!")
+// })
 // app.use("/", (req, res) => {
 //     res.send("Hello from Server!")
 // })
 
-app.use("/test", (req, res) => {
-    res.send("Hello from Test Server!")
+app.get("/user", (req, res) =>{
+    res.send({firstName: "Ram", lastName: "Pandey"});
+});
+
+app.post("/user", (req, res)=>{
+    res.send("Data Saved to DB Successfully!");
+});
+
+app.put("/user", (req, res) => {
+    res.send("Data Updated Successfully")
+});
+app.patch("/user", (req, res)=>{
+    res.send("Data Field updated Successfully")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data Deleted from DB Successfully")
 })
 
 app.listen(PORT, () =>{
