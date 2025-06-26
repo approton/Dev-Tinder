4c8e049f34ff4668d7dc6afa7f2f4f05a1f0ca4b
Ram Niwash Pandey <pandey.ram1397@gmail.com>
2025-06-26 11:04:27 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -14,25 +14,53 @@ const PORT = 3000;
 //     res.send("Hello from Server!")
 // })
 
-app.get("/user", (req, res) =>{
-    res.send({firstName: "Ram", lastName: "Pandey"});
-});
+// app.get("/user", (req, res) =>{
+//     console.log(req.query)
+//     res.send({firstName: "Ram", lastName: "Pandey"});
+// });
 
-app.post("/user", (req, res)=>{
-    res.send("Data Saved to DB Successfully!");
-});
+// app.post("/user", (req, res)=>{
+//     res.send("Data Saved to DB Successfully!");
+// });
 
-app.put("/user", (req, res) => {
-    res.send("Data Updated Successfully")
-});
-app.patch("/user", (req, res)=>{
-    res.send("Data Field updated Successfully")
-});
+// app.put("/user", (req, res) => {
+//     res.send("Data Updated Successfully")
+// });
+// app.patch("/user", (req, res)=>{
+//     res.send("Data Field updated Successfully")
+// });
 
-app.delete("/user", (req, res) => {
-    res.send("Data Deleted from DB Successfully")
-})
+// app.delete("/user", (req, res) => {
+//     res.send("Data Deleted from DB Successfully")
+// })
 
+// app.get("/user/:userId/:username/:password", (req, res)=>{
+//     console.log(req.params);
+//     res.send("Complex Regex Pattern")
+// })
+
+app.use("/user",
+    (req, res, next)=>{
+        console.log("1st Route Handler");
+        next();
+    },
+    (req, res, next)=>{
+        console.log("2nd Route Handler");
+        next();
+    },
+    (req, res, next)=>{
+        console.log("3rd Route Handler");
+        next();
+    },
+    (req, res, next)=>{
+        console.log("4th Route Handler");
+        next();
+    },
+    (req, res, next)=>{
+        console.log("5th Route Handler");
+        res.send("5th Handler")
+    },
+)
 app.listen(PORT, () =>{
     console.log("Server Started on PORT: ", PORT)
 })
